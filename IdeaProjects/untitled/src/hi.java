e79e22b3863df46cc9b878f73457716bceed0bc0
LOGANANTH234 <logananthayyasamy@gmail.com>
2023-01-26 00:25:37 +0530

    updated

---
+++ IdeaProjects/untitled/src/hi.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class hi {
+void a (){
+    System.out.println("gyfrdfd");
+}
+
+    public static void main(String[] args) {
+bye b=new bye();
+b.m();
+b.a();
+
+
+    }
+}
